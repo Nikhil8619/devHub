e9152f0bc8a9e77e28ae7fd8efd5fb83ce547342
Nikhil8619 <nikhilagrawal546@gmail.com>
2025-07-27 21:39:32 +0530

    dynamic routing, regex, req.query, req,params explored

--- src/app.js
+++ src/app.js
@@ -1,39 +1,27 @@
 const express=require("express");
 const app=express();
 
-app.get("/user",(req,res)=>{
+app.get("/user/:userId",(req,res)=>{
+    // console.log(req.query);
+    console.log(req.params);
     res.send({firstname:"Nikhil", lastname:"Agrawal"})
 })
 
-app.post("/user",(req,res)=>{
-   res.send("Data has been successfully added to then SDB");
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("Data has been successfully Deleted to then SDB");
-})
-
-app.use("/test/12",(req,res)=>{
-    res.send("AbrakaDabra gili gili chuu");
-});
-
-app.use("/test",(req,res)=>{
-    res.send("This is our test routing without fail using get function");
-});
-
-// app.use("/hello",(req,res)=>{
-//     res.send("hello  ");
+// app.post("/user",(req,res)=>{
+//    res.send("Data has been successfully added to then SDB");
 // })
 
-// app.use((req,res)=>{
-//     res.send("Welcome to the port")
+// app.delete("/user",(req,res)=>{
+//     res.send("Data has been successfully Deleted to then SDB");
 // })
 
-// app.use("/",(req,res)=>{
-//     res.send("Hello welcome to port 7777");
+// app.use("/test/12",(req,res)=>{
+//     res.send("AbrakaDabra gili gili chuu");
 // });
 
-
+// app.use("/test",(req,res)=>{
+//     res.send("This is our test routing without fail using get function");
+// });
 
 app.listen(7777,()=>{
     console.log("server has started running on port 7777");
