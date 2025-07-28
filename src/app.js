8a280190b921ba6190fa89bfa52cc1d7c61b386b
Nikhil8619 <nikhilagrawal546@gmail.com>
2025-07-28 08:41:01 +0530

    multi-routing-paths and use of array example

--- src/app.js
+++ src/app.js
@@ -1,27 +1,33 @@
 const express=require("express");
 const app=express();
 
-app.get("/user/:userId",(req,res)=>{
-    // console.log(req.query);
-    console.log(req.params);
-    res.send({firstname:"Nikhil", lastname:"Agrawal"})
-})
-
-// app.post("/user",(req,res)=>{
-//    res.send("Data has been successfully added to then SDB");
-// })
-
-// app.delete("/user",(req,res)=>{
-//     res.send("Data has been successfully Deleted to then SDB");
-// })
-
-// app.use("/test/12",(req,res)=>{
-//     res.send("AbrakaDabra gili gili chuu");
-// });
-
-// app.use("/test",(req,res)=>{
-//     res.send("This is our test routing without fail using get function");
-// });
+app.use("/user",
+    [(req,res,next)=>{
+    console.log("handling  the route user");
+    // res.send("Response");
+    next();
+},
+ (req,res,next)=>{
+    console.log("handling  the route user 2");
+    // res.send("Response 2");
+    next();
+}],
+(req,res,next)=>{
+    console.log("handling  the route user 3");
+    // res.send("Response 3");
+    next();
+},
+(req,res,next)=>{
+    console.log("handling  the route user 4");
+    // res.send("Response 4");
+    next();
+},
+(req,res)=>{
+    console.log("handling  the route user 5");
+    res.send("Response 5");
+    
+}
+)
 
 app.listen(7777,()=>{
     console.log("server has started running on port 7777");
