83e1a9ed9b6f130fac61a2ae609af3c765d01e83
Nikhil8619 <nikhilagrawal546@gmail.com>
2025-08-19 07:30:48 +0530

    password update api created

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,7 @@ const express=require("express");
 const profileRouter=express.Router();
 const {userAuth}=require("../middleware/auth");
 const {validateEditProfileData}=require("../utils/validation")
+const bcrypt=require("bcrypt");
 
 profileRouter.get("/profile/view",userAuth,async(req,res)=>{
   try{
@@ -33,6 +34,32 @@ profileRouter.patch("/profile/edit", userAuth, async(req,res)=>{
   }
   catch(err){
     res.status(400).send("ERROR " + err.message);
+  }
+})
+
+profileRouter.patch("/profile/password",userAuth,async(req,res)=>{
+  try{
+    const {oldPassword,newPassword}=req.body;
+    if(!oldPassword || !newPassword){
+      throw new Error("Old and new password must be provided");
+    }
+
+    const user=req.user;
+    const isMatch=await bcrypt.compare(oldPassword,user.passWord);
+    if(!isMatch){
+      throw new Error("password is incorrect");
+    }
+
+    const salt=await bcrypt.genSalt(10);
+    user.passWord=await bcrypt.hash(newPassword,salt);
+
+    await user.save();
+    res.json({ message: "Password updated successfully!" });
+
+  }catch(err){
+    res.status(400).json({
+      message: err.message,
+    })
   }
 })
 
